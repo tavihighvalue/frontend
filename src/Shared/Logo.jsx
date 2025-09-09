d61ab6e79948e7520259d45576927218e194b47f
tavihighvalue <tavihighvalue@outlook.com>
2025-09-09 10:17:22 +0200

    Delete src directory

--- src/Shared/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import LogoImg from '../assets/logo.png'; // Or PNG
-
-const Logo = ({ size = 32 }) => (
-  <Box component="img" src={LogoImg} alt="Logo" height={size} />
-);
-
-export default Logo;
--- src/Shared/ThemeIcon.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-
-export function ThemeIcon({ Icon, size = 20, color, ...props }) {
-  const { theme } = useContext(ThemeContext);
-  return <Icon size={size} color={color || theme.palette.text.primary} {...props} />;
-}
